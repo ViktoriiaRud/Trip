368d463b5d47e387d96be65ee88055a8de24e098
Viktoriia <rudneva15v@gmail.com>
2022-12-04 09:05:38 +0100

    add exclusiveData

--- src/components/organisms/exclusiveOffers/ExclusiveOffers.js
+++ src/components/organisms/exclusiveOffers/ExclusiveOffers.js
@@ -5,24 +5,26 @@ import BitmapVilaImg from '../../../assets/img/BitmapVila.png';
 
 import { Exclusive, WrapperCards, WrapperItem, BackgroundOrange, ExclusiveBackgroundColor } from './ExclusiveOffers.styles'
 import { ExclusiveSubTitle, ExclusiveTitle, ExclusiveCityTitle, ExclusiveCountryTitle, ExclusiveDollar } from '../../atoms/fontSize/FontSize.styles';
+import exclusiveData from './exclusiveData';
 
+const { subTitle, title, city, country, dollar1, dollar2, dollar3 } = exclusiveData;
 
 const ExclusiveOffers = () => {
 
     return <>
         <ExclusiveBackgroundColor>
             <Exclusive >
-                <ExclusiveSubTitle>EXCLUSIVE OFFERS</ExclusiveSubTitle>
-                <ExclusiveTitle>Discover Popular Destinations</ExclusiveTitle>
+                <ExclusiveSubTitle>{subTitle}</ExclusiveSubTitle>
+                <ExclusiveTitle>{title}</ExclusiveTitle>
                 <WrapperCards>
                     <WrapperItem>
                         <div className="BackgroundPhoto">
                             <img src={VilaImg} alt="VilaImg" />
-                            <ExclusiveCityTitle>Rome</ExclusiveCityTitle>
-                            <ExclusiveCountryTitle>Italy</ExclusiveCountryTitle>
+                            <ExclusiveCityTitle>{city}</ExclusiveCityTitle>
+                            <ExclusiveCountryTitle>{country}</ExclusiveCountryTitle>
                         </div>
                         <BackgroundOrange className="BackgroundOrange">
-                            <ExclusiveDollar>$199</ExclusiveDollar>
+                            <ExclusiveDollar>{dollar1}</ExclusiveDollar>
                         </BackgroundOrange>
                     </WrapperItem>
 
@@ -33,7 +35,7 @@ const ExclusiveOffers = () => {
                             <ExclusiveCountryTitle>USA</ExclusiveCountryTitle>
                         </div>
                         <BackgroundOrange className="BackgroundOrange">
-                            <ExclusiveDollar>$250</ExclusiveDollar>
+                            <ExclusiveDollar>{dollar2}</ExclusiveDollar>
                         </BackgroundOrange>
                     </WrapperItem>
 
@@ -44,7 +46,7 @@ const ExclusiveOffers = () => {
                             <ExclusiveCountryTitle>France</ExclusiveCountryTitle>
                         </div>
                         <BackgroundOrange className="BackgroundOrange">
-                            <ExclusiveDollar>$129</ExclusiveDollar>
+                            <ExclusiveDollar>{dollar3}</ExclusiveDollar>
                         </BackgroundOrange>
                     </WrapperItem>
                 </WrapperCards>
---
+++ src/components/organisms/exclusiveOffers/exclusiveData.js
@@ -0,0 +1,12 @@
+
+export const exclusiveData = {
+    subTitle: "EXCLUSIVE OFFERS",
+    title: "Discover Popular Destinations",
+    city: "Rome",
+    country: "Italy",
+    dollar1: "$199",
+    dollar2: "$250",
+    dollar3: "$129"
+}
+
+export default exclusiveData;
